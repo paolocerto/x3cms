3fb9a7c0730a75a023a4f857765aa4861a9e42de
Paolo Certo <paolocerto@cblu.net>
2016-10-13 19:53:27 +0200

    added privtype to x4site_map plugin to handle permission on it

--- plugins/x4site_map/install.php
+++ plugins/x4site_map/install.php
@@ -26,5 +26,12 @@ $required = array();
 // sql1 is the array of queries for specific area use (parameters, dictionary, module)
 $sql0 = $sql1 = array();
 
+// administration priv
+$sql0[] = "INSERT INTO privtypes (updated, xrif, name, description, xon) VALUES (NOW(), 1, 'x4_site_map', 'X4_SITEMAP', 1)";
+$sql0[] = "INSERT INTO gprivs (updated, id_group, what, level, xon) VALUES (NOW(), 1, 'x4_site_map', 4, 1)";
+
+$sql0[] = "INSERT INTO dictionary (updated, lang, area, what, xkey, xval, xlock, xon) VALUES (NOW(), 'it', 'admin', 'groups', 'X4_SITEMAP', 'Gestione mappa del sito', 0, 1)";
+$sql0[] = "INSERT INTO dictionary (updated, lang, area, what, xkey, xval, xlock, xon) VALUES (NOW(), 'en', 'admin', 'groups', 'X4_SITEMAP', 'Gestione mappa del sito', 0, 1)";
+
 // module
 $sql1[] = "INSERT INTO modules (updated, id_area, name, description, configurable, admin, searchable, pluggable, version, xon) VALUES (NOW(), $id_area, '$mod_name', 'Area map', 0, 0, 0, 1, $version, 0)";
