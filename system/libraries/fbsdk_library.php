c001ba9c6cd7d1936cd8f882241bbf68329d6a42
cblu <paolocerto@gmail.com>
2020-02-05 17:19:30 +0100

    removed obsolete libraries

--- system/libraries/fbsdk_library.php
+++
@@ -1,9 +0,0 @@
-<?php defined('ROOT') or die('No direct script access.');
-/**
- * FB SDK
- */
- 
-// to use this you need to download the Facebook SDK and put it in the facebook_sdk folder in system/libraries
-require_once(dirname(__FILE__).'/facebook_sdk/autoload.php');
-
-
