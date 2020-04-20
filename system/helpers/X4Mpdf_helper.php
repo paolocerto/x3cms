a19bd0b1c937842bc486a05b1f1fcb780c431fb8
cblu <paolocerto@gmail.com>
2020-04-20 12:48:47 +0200

    modified mpdf helper to use vendor folder

--- system/helpers/X4Mpdf_helper.php
+++ system/helpers/X4Mpdf_helper.php
@@ -24,59 +24,10 @@ class X4Mpdf_helper
 	 * @param	string	$html		HTML Contents
 	 * @param	string	$page_format 	Default A4
 	 * @param	string	$orientation	Can be P|L
-	 * @return boolean
-	 */
-	public static function pdf_export($title, $css, $html, $page_format = 'A4', $orientation = 'P')
-	{
-		// language set
-		$l = array();
-		$l['a_meta_charset'] = 'UTF-8';
-		$l['a_meta_dir'] = 'rtl';
-		$l['a_meta_language'] = X4Route_core::$lang;
-		$l['w_page'] = _PAGE;
-		
-		X4Core_core::auto_load('mpdf_vendor');
-		
-		// create the PDF object 
-		$mpdf = new \Mpdf\Mpdf([
-		    'mode' => 'utf-8', 
-		    'format' => $page_format, 
-		    'orientation' => $orientation
-		]);
-		
-		// Set a simple Footer including the page number
-		$mpdf->setFooter('{PAGENO}');
-				
-		$title = SERVICE.' - '.$title.' - '.date('Y-m-d H:i:s');
-		
-		$mpdf->SetAuthor($_SESSION['nickname']);
-		$mpdf->SetCreator(SERVICE);
-		$mpdf->SetTitle($title);
-		
-		$mpdf->SetDisplayMode('fullwidth');
-		
-		$mpdf->WriteHTML($css ,1);
-		$mpdf->WriteHTML($html, 2);
-		
-		$filename = X4Utils_helper::unspace(str_replace(' - ', '-', $title), true);
-		
-		$mpdf->Output($filename.'.pdf', 'D');
-		exit;
-	}
-	
-	/**
-	 * Export a pdf
-	 *
-	 * @static
-	 * @param	string	$title		Document title
-	 * @param	string	$css		CSS Contents
-	 * @param	string	$html		HTML Contents
-	 * @param	string	$page_format 	Default A4
-	 * @param	string	$orientation	Can be P|L
-	 * @param	string	$path	        If empty you get a download, if not empty the file will be saved in the path
+	 * @param	string	$path			Can be download|path
 	 * @return mixed
 	 */
-	public static function pdf_margined($title, $css, $html, $page_format = 'A4', $orientation = 'P', $footer = '')
+	public static function pdf_export($title, $css, $html, $page_format = 'A4', $orientation = 'P', $path = '')
 	{
 		// language set
 		$l = array();
@@ -87,13 +38,6 @@ class X4Mpdf_helper
 		
 		X4Core_core::auto_load('mpdf_vendor');
 		
-		$pf = ($orientation == 'P')
-		    ? $page_format
-		    : $page_format.'-'.$orientation;
-		    
-		// create the PDF object 
-		//$mpdf = new mPDF(X4Route_core::$lang, $pf, 9, '', 5, 5, 10, 20, 5, 5);
-		
 		// create the PDF object 
 		$mpdf = new \Mpdf\Mpdf([
 		    'mode' => 'utf-8', 
@@ -102,14 +46,11 @@ class X4Mpdf_helper
 		]);
 		
 		// Set a simple Footer including the page number
-		$mpdf->setFooter($footer.'{PAGENO}/{nbpg}');
-				
-		if (empty($path))
-		{
-		    $title = SERVICE.' - '.$title.' - '.date('Y-m-d H:i:s');
-		}
+		$mpdf->setFooter('{PAGENO}/{nbpg}');
 		
-		$mpdf->SetAuthor($_SESSION['nickname']);
+		$title = SERVICE.' - '.$title.' - '.date('Y-m-d H:i:s');
+		
+		$mpdf->SetAuthor(SERVICE);
 		$mpdf->SetCreator(SERVICE);
 		$mpdf->SetTitle($title);
 		
@@ -122,15 +63,16 @@ class X4Mpdf_helper
 		
 		if (empty($path))
 		{
-		    $mpdf->Output($filename.'.pdf', 'D');
-		    exit;
+			//download
+			$mpdf->Output($filename.'.pdf', 'D');
+			exit;
 		}
 		else
 		{
+			// save
 		    $mpdf->Output($path.$filename.'.pdf', 'F');
 		    return $path.$filename.'.pdf';
 		}
 	}
 }
-
 
