508b70290da0a7704df8597bd5197c653735a637
Paolo Certo <paolocerto@gmail.com>
2023-09-16 16:10:56 +0200

    fixed wrong style for widgets container

--- cms/views/admin/home_view.php
+++ cms/views/admin/home_view.php
@@ -11,14 +11,14 @@
 <h1><?php echo _ADMIN_AREA ?></h1>
 <p><?php echo _HI.' <strong> '.$_SESSION['username'].'</strong>, '._LAST_LOGIN.' '.$_SESSION['last_in'] ?></p>
 
-<div class="mt-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 place-items-stretch space-x-4 mb-20">
+<div class="mt-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-20 mb-20">
 <?php
 // notices from x3cms.net
 if (NOTICES)
 {
 	echo '<div>
             <div class="bg rounded-t px-4 py-4"><h4>'._NOTICES_AND_UPDATES.'</h4></div>
-            <div class="bg2 h-full px-4 pt-4 pb-8">'.$notices.'</div>
+            <div class="bg2 h-full px-4 pt-4 pb-8 mb-4">'.$notices.'</div>
         </div>';
 }
 
